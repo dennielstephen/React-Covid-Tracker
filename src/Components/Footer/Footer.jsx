0a20c29aec72e1ee2251b50920faaf6011bfa2f2
Denniel Stephen Mordeno <dennielstephen@gmail.com>
2021-02-03 19:57:34 +0800

    initial commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+
+  return (
+    <React.Fragment>
+      <div className={styles.container}>
+        <h6
+          style={{
+            display: "block",
+            marginBottom: "0px",
+            fontSize: 30,
+            marginTop: "40px",
+            textAlign: "center",
+          }}
+        >
+          Created with care by{" "}
+          <a
+            href="https://github.com/dennielstephen"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "rgba(0,0,255,0.6)", textDecoration: "none" }}
+          >
+            Denniel Stephen Mordeno
+          </a>
+        </h6>
+      </div>
+      <div className={styles.container}>
+        <h6
+          style={{
+            display: "block",
+            marginBottom: "0px",
+            fontSize: 16,
+            marginTop: "20px",
+            textAlign: "center",
+          }}
+        >
+          Data provided 💖 by{" "}
+          <a
+            href="https://github.com/mathdroid/covid-19-api"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "rgba(0,0,255,0.6)", textDecoration: "none" }}
+          >
+            Mathdroid
+          </a>
+        </h6>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Footer;
